cf5ef7fcc7446a6f9def2957697c4ffeb0a6b89a
nmcode <nmcode.personal@gmail.com>
2022-08-02 20:27:32 +0300

    создание комментариев

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentEntity } from './comment.entity';
+import { Repository } from 'typeorm';
+import { CommentDto } from './dto/comment.dto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+	constructor(
+		@InjectRepository(CommentEntity)
+		private commentRepository: Repository<CommentEntity>,
+	) {}
+
+	async create(userId: number, dto: CommentDto) {
+		const newComment = await this.commentRepository.create({
+			message: dto.message,
+			video: { id: dto.videoId },
+			user: {
+				id: userId,
+			},
+		});
+
+		return this.commentRepository.save(newComment);
+	}
+}
